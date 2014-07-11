8fdf55116646e048ca1b793e9728658fa6d43b9e
Felippe Piazza <felippe@felippe.org>
2014-07-11 15:08:10 -0300

    capistrano changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,47 +2,46 @@
 lock '3.2.1'
 
 set :application, 'yq'
-set :repo_url, 'git@github.com:felippefpiazza/yq.git'
+set :repository, 'git@github.com:felippefpiazza/yq.git'
+set :base_path, '/var/www'
+role :web, "104.131.233.143"
+role :app, "104.131.233.143"
+role :db, "104.131.233.143", :primary => true
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, "master"
+#####################################################################
+#### normally two deployment environments ###########################
+#####################################################################
 
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/www/yq.com.br'
+set :targets, {
+  teste:      'test',
+  production: 'prod'
+}
 
-# Default value for :scm is :git
+targets.each do |target, app|
+  task target.to_sym do
+    set :app_namespace, "#{application}_#{app}"
+    set :deploy_to, "#{base_path}/#{app_namespace}"
+  end
+end
+
+#####################################################################
+#### there shouldnt be any need to change things below this line ####
+#####################################################################
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :rvm_ruby_string, '2.1.2'
+set :rvm_type, :system 
 set :scm, :git
+set :deploy_via, :export
+set :use_sudo, false
+set :user, "felippe"
 
-# Default value for :format is :pretty
-# set :format, :pretty
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+#set :rails_env, "production"
+#set :keep_releases, 5
 
-# Default value for :pty is false
-# set :pty, true
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-set :use_sudo , "false"
-set :rails_env, "production"
-set :deploy_via, :copy
-set :keep_releases, 5
-server "104.131.233.143", user: "felippe", roles: %w{app web db}
-#role :web, "104.131.233.143"
-#role :app, "104.131.233.143"
-#role :db, "104.131.233.143", :primary => true
 
 namespace :deploy do
 
